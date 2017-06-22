77c0242d585b0a5396ab0f10ad29c907641ea778
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-06-23 00:15:55 +0500

    Added 404 Page

---
+++ views/errors/404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 - NOT FOUND</title>
+</head>
+<body>
+    <center><h2>404 - PAGE NOT FOUND!</h2></center>
+</body>
+</html>
